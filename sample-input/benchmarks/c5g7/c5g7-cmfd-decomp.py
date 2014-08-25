9ff6105c8143db0b0985a5397cbee1f9e5a67c11
Sam Shaner <shaner@mit.edu>
2014-08-25 10:35:16 -0700

    modified plotter

--- sample-input/benchmarks/c5g7/c5g7-cmfd-decomp.py
+++ sample-input/benchmarks/c5g7/c5g7-cmfd-decomp.py
@@ -343,7 +343,7 @@ geometry.initializeFlatSourceRegions()
 log.py_printf('NORMAL', 'Initializing the track generator...')
 
 track_generator = ModularTrackGenerator(geometry, num_azim, track_spacing)
-track_generator.setLatticeStructure(3,3)
+track_generator.setLatticeStructure(1,1)
 track_generator.generateTracks()
 
 ###############################################################################
